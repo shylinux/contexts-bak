0afddcaeba3685e09e5cea48f9cbe961467dee6d
shaoying <shaoying@zuoyebang.com>
2020-05-07 11:33:42 +0800

    opt etc/miss.sh

--- etc/miss.sh
+++ etc/miss.sh
@@ -2,13 +2,20 @@
 [ "$ISH_CONF_PRE" != "" ] || source ./.ish/plug.sh || source ~/.ish/plug.sh
 # declare -f ish_help_repos &>/dev/null || require conf.sh
 
+require show.sh
 require help.sh
 require miss.sh
+cd $ISH_CONF_TASK
 
 ish_miss_prepare
-ish_miss_compile_prepare
-ish_miss_install_prepare
+ish_miss_prepare_compile
+ish_miss_prepare_install
 
-ish_miss_volcanos_prepare
-ish_miss_icebergs_prepare
-ish_miss_intshell_prepare
+ish_miss_prepare_volcanos
+ish_miss_prepare_icebergs
+ish_miss_prepare_intshell
+ish_miss_prepare_toolkits
+ish_miss_prepare_learning
+
+ish_miss_prepare wubi-dict
+ish_miss_prepare word-dict
