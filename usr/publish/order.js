591bea29a83fae0e17d4d235a9cabad47413b6e1
shylinux <shylinux@icloud.com>
2020-06-11 10:48:01 +0800

    opt order

--- usr/publish/order.js
+++ usr/publish/order.js
@@ -1,36 +1,18 @@
 Volcanos("onengine", { river: {
     "main": {name: "main", storm: {
-        "main": {name: "main", action: [
-            {name: "IDE", help: "集成开发环境", inputs: [
-                {type: "text", name: "path", value: "tmp", action: "auto"},
-                {type: "text", name: "name", value: "hi.qrc", action: "auto"},
-                {type: "text", name: "key", value: "", action: "auto"},
-                {type: "button", name: "打开", action: "auto"},
-                {type: "button", name: "返回"},
-                {type: "button", name: "历史"},
-                {type: "button", name: "复盘"},
-            ], index: "web.wiki.inner", feature: {display: "/plugin/inner.js", style: "editor"}},
-            {name: "word", help: "文档", inputs: [
-                {type: "text", name: "path", value: "自然/编程/hi.shy", action: "auto"},
-                {type: "button", name: "查看", action: "auto"},
-                {type: "button", name: "返回"},
-            ], index: "web.wiki.word", feature: {display: "/plugin/local/wiki/word.js", style: "word"}},
+        "main": {name: "main", index: [
+            "web.wiki.inner",
+            "web.code.git.status",
         ]},
         "task": {name: "task", index: [
             "web.team.plan",
-            "web.team.task",
-            "web.team.miss",
-            "web.code.git.status",
+            "web.wiki.word",
         ]},
         "relay": {name: "relay", index: [
             "aaa.totp.get",
+            "web.route",
         ]},
         "hello": {name: "应用1", action: [
-            {name: "route", help: "路由", inputs: [
-                {type: "text", name: "name", value: "", action: "auto"},
-                {type: "text", name: "cmd"},
-                {type: "button", name: "查看", action: "auto"},
-            ], index: "web.route"},
             {name: "some", help: "some", inputs: [
                 {type: "text", name: "one"},
                 {type: "button", name: "one"},
@@ -39,12 +21,10 @@ Volcanos("onengine", { river: {
                 msg.Echo("hello world");
                 typeof cb == "function" && cb(msg);
             }},
-        ]},
-        "world": {name: "应用2", action: [
             {name: "hello", help: "world", inputs: [
                 {type: "text", name: "one", value: "pwd"},
                 {type: "button", name: "one"},
-            ], group: "cli", index: "system"},
+            ], index: "cli.system"},
         ]},
     }},
 }, })
