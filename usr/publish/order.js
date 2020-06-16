efa05887ec4cba2f0237e9f54eccc66952a677dd
shaoying <shylinux@icloud.com>
2020-06-16 09:44:26 +0800

    opt some

--- usr/publish/order.js
+++ usr/publish/order.js
@@ -16,8 +16,9 @@ Volcanos("onengine", { river: {
         "性能": {name: "性能", index: [
             "web.code.bench",
             "web.code.pprof",
-            "web.favor",
             "web.share",
+            "web.favor",
+            "web.cache",
         ]},
         "trans": {name: "翻译", action: [
             {name: "english", help: "英文", inputs: [
