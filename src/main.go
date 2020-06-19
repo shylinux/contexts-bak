ccad228cfb316e35f14e9950760806d98ab587c1
shylinux <shylinux@icloud.com>
2020-06-19 08:41:47 +0800

    mix some

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/shylinux/icebergs/misc/alpha"
 	_ "github.com/shylinux/icebergs/misc/chrome"
 	_ "github.com/shylinux/icebergs/misc/input"
-	// _ "github.com/shylinux/icebergs/misc/lark"
+	_ "github.com/shylinux/icebergs/misc/lark"
 	// _ "github.com/shylinux/icebergs/misc/md"
 	// _ "github.com/shylinux/icebergs/misc/mp"
 	// _ "github.com/shylinux/icebergs/misc/pi"
