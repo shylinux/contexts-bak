8e3feecfc915a955cd0768b6a686367919a461f2
shylinux <shylinux@icloud.com>
2020-08-12 10:33:16 +0800

    opt some

--- src/main.go
+++ src/main.go
@@ -15,19 +15,13 @@ import (
 	_ "github.com/shylinux/icebergs/misc/lark"
 	_ "github.com/shylinux/icebergs/misc/mp"
 	_ "github.com/shylinux/icebergs/misc/wx"
-	// _ "github.com/shylinux/icebergs/misc/md"
-	// _ "github.com/shylinux/icebergs/misc/pi"
-	//
-	// _ "github.com/shylinux/icebergs/misc/fyne"
-
-	_ "github.com/shylinux/redis-story/src/client"
-	_ "github.com/shylinux/redis-story/src/server"
 
 	_ "github.com/shylinux/golang-story/src/compile"
 	_ "github.com/shylinux/golang-story/src/project"
 	_ "github.com/shylinux/golang-story/src/runtime"
-
 	_ "github.com/shylinux/nginx-story/src/server"
+	_ "github.com/shylinux/redis-story/src/client"
+	_ "github.com/shylinux/redis-story/src/server"
 )
 
 func main() { ice.Run() }
