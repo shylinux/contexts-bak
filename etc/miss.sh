43515b3d7cb4d693883979d3569aa686527f8d9d
shaoying <shy@172.16.133.132>
2020-08-30 02:30:08 -0400

    opt some

--- etc/miss.sh
+++ etc/miss.sh
@@ -3,26 +3,17 @@
 
 [ -f ~/.ish/plug.sh ] || [ -f ./.ish/plug.sh ] || git clone https://github.com/shylinux/intshell ./.ish
 [ "$ISH_CONF_PRE" != "" ] || source ./.ish/plug.sh || source ~/.ish/plug.sh
-
-require show.sh
-require help.sh
 require miss.sh
 
-# ish_miss_prepare_compile
-# ish_miss_prepare_install
-# ish_miss_prepare_develop
-ish_miss_prepare_session miss
+ish_miss_prepare_compile
+ish_miss_prepare_install
 
 ish_miss_prepare_volcanos
 ish_miss_prepare learning
 ish_miss_prepare_icebergs
 ish_miss_prepare toolkits
-ish_log_debug "require as ctx github.com/shylinux/intshell"
 ish_miss_prepare_intshell
-ish_log_debug "require as ctx github.com/shylinux/contexts"
-git pull
-pwd
-
+ish_miss_prepare_contexts
 
 # ish_miss_prepare wubi-dict
 # ish_miss_prepare word-dict
@@ -34,11 +25,34 @@ ish_miss_prepare redis-story
 ish_miss_prepare mysql-story
 
 ish_miss_prepare_vim() {
+    ish_miss_create_link ~/.vimrc $PWD/etc/conf/vimrc
+    ish_miss_create_link ~/.auto.vim $PWD/usr/publish/auto.vim
+
+    mkdir -p ~/.vim/autoload
+    ish_miss_create_link ~/.vim/autoload/plug.vim $PWD/etc/conf/plug.vim
+
     mkdir -p ~/.vim/syntax
-    [ -f ~/.vim/syntax/sh.vim ] || ln etc/conf/sh.vim ~/.vim/syntax/sh.vim
-    [ -f ~/.vim/syntax/shy.vim ] || ln etc/conf/shy.vim ~/.vim/syntax/shy.vim
-    [ -f ~/.vim/syntax/go.vim ] || ln etc/conf/go.vim ~/.vim/syntax/go.vim
-    [ -f ~/.vim/syntax/javascript.vim ] || ln etc/conf/javascript.vim ~/.vim/syntax/javascript.vim
+    ish_miss_create_link ~/.vim/syntax/sh.vim $PWD/etc/conf/sh.vim
+    ish_miss_create_link ~/.vim/syntax/shy.vim $PWD/etc/conf/shy.vim
+    ish_miss_create_link ~/.vim/syntax/go.vim $PWD/etc/conf/go.vim
+    ish_miss_create_link ~/.vim/syntax/javascript.vim $PWD/etc/conf/javascript.vim
 }
+ish_miss_prepare_git() {
+    git --global config alias.s status
+    git --global config alias.b branch
+}
+ish_miss_prepare_tmux() {
+    ish_miss_create_link ~/.tmux.conf $PWD/etc/conf/tmux.conf
+}
+ish_miss_prepare_bash() {
+    ish_miss_create_link ~/.bashrc $PWD/etc/conf/bashrc
+    ish_miss_create_link ~/.ish $PWD/.ish
+}
+
+ish_miss_prepare_bash
+ish_miss_prepare_tmux
+ish_miss_prepare_git
 ish_miss_prepare_vim
 
+ish_miss_prepare_develop
+ish_miss_prepare_session miss
--- src/main.go
+++ src/main.go
@@ -11,7 +11,6 @@ import (
 	_ "github.com/shylinux/icebergs/misc/input"
 	_ "github.com/shylinux/icebergs/misc/totp"
 
-	_ "github.com/shylinux/icebergs/misc/es"
 	_ "github.com/shylinux/icebergs/misc/lark"
 	_ "github.com/shylinux/icebergs/misc/mp"
 	_ "github.com/shylinux/icebergs/misc/wx"
@@ -20,24 +19,23 @@ import (
 	_ "github.com/shylinux/golang-story/src/project"
 	_ "github.com/shylinux/golang-story/src/runtime"
 
-	_ "github.com/shylinux/linux-story/src/android"
-	_ "github.com/shylinux/linux-story/src/centos"
-	_ "github.com/shylinux/linux-story/src/context"
-	_ "github.com/shylinux/linux-story/src/ubuntu"
-
+	_ "github.com/shylinux/linux-story/iso/android"
+	_ "github.com/shylinux/linux-story/iso/centos"
+	_ "github.com/shylinux/linux-story/iso/context"
+	_ "github.com/shylinux/linux-story/iso/ubuntu"
+	_ "github.com/shylinux/linux-story/src/busybox"
 	_ "github.com/shylinux/linux-story/src/gcc"
 	_ "github.com/shylinux/linux-story/src/gdb"
 	_ "github.com/shylinux/linux-story/src/glibc"
-
-	_ "github.com/shylinux/linux-story/src/busybox"
 	_ "github.com/shylinux/linux-story/src/kernel"
 	_ "github.com/shylinux/linux-story/src/qemu"
-
-	_ "github.com/shylinux/mysql-story/src/client"
-	_ "github.com/shylinux/mysql-story/src/server"
 	_ "github.com/shylinux/nginx-story/src/server"
 	_ "github.com/shylinux/redis-story/src/client"
 	_ "github.com/shylinux/redis-story/src/server"
+
+	_ "github.com/shylinux/mysql-story/src/client"
+	_ "github.com/shylinux/mysql-story/src/search"
+	_ "github.com/shylinux/mysql-story/src/server"
 )
 
 func main() { ice.Run() }
