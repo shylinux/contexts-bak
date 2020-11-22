f9eb42741e8021334d8a9ed2e2fb3fa730bcb04a
shaoying <shylinux@icloud.com>
2020-11-22 17:55:15 +0800

    opt some

--- etc/miss.sh
+++ etc/miss.sh
@@ -24,25 +24,6 @@ ish_miss_prepare golang-story
 ish_miss_prepare redis-story
 ish_miss_prepare mysql-story
 
-ish_miss_prepare_bash() {
-    ish_miss_create_link ~/.bashrc $PWD/etc/conf/bashrc
-    ish_miss_create_link ~/.zshrc $PWD/etc/conf/zshrc
-    ish_miss_create_link ~/.ish $PWD/.ish
-
-    if [ -z `cat ~/.bashrc |grep "source ~/.ish/plug.sh"` ]; then
-        if [ -z `cat ~/.bashrc |grep "source $PWD/etc/conf/bashrc"` ]; then
-            echo -e "\nsource $PWD/etc/conf/bashrc\n" >> ~/.bashrc
-        fi
-    fi
-
-    if [ -z `cat ~/.zshrc |grep "source ~/.ish/plug.sh"` ]; then
-        if [ -z `cat ~/.zshrc |grep "source $PWD/etc/conf/zshrc"` ]; then
-            echo -e "\nsource $PWD/etc/conf/zshrc\n" >> ~/.zshrc
-        fi
-    fi
-}
-# ish_miss_prepare_bash
-
 require misc/tmux/tmux.sh
 require misc/git/git.sh
 require misc/vim/vim.sh
@@ -51,6 +32,5 @@ ish_ctx_dev_tmux_prepare
 ish_ctx_dev_git_prepare
 ish_ctx_dev_vim_prepare
 
-# ish_miss_prepare_develop
 make
 ish_miss_prepare_session miss
