2b5866a5559478d65fa520fdf04ece5e9b6d2b83
shaoying <shaoying@zuoyebang.com>
2020-11-26 21:50:22 +0800

    opt some

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ export CGO_ENABLED=0
 all:
 	@echo && date
 	go build -v -o bin/ice.bin src/main.go && chmod u+x bin/ice.bin && ./bin/ice.sh restart
+relay: src/relay.go
+	@echo && date
+	go build -v -o usr/publish/$@ src/$@.go && chmod u+x usr/publish/$@
--- src/relay.go
+++ src/relay.go
@@ -3,14 +3,15 @@ package main
 import (
 	ice "github.com/shylinux/icebergs"
 	_ "github.com/shylinux/icebergs/base/ssh"
+	log "github.com/shylinux/toolkits/logs"
 
 	"os"
 	"path"
 )
 
 func main() {
-	args := []string{"ssh.connect", "open", "authfile", path.Join(os.Getenv("HOME"), ".ssh/" path.Base(os.Args[0])".json")}
+	log.LogDisable = true
+	args := []string{"ssh.connect", "open", "authfile", path.Join(os.Getenv("HOME"), ".ssh/", path.Base(os.Args[0])+".json")}
 	args = append(args, os.Args[1:]...)
 	print(ice.Run(args...))
 }
-
