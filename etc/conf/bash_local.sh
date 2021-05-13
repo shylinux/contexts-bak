1557fbae38cb7990f3799786bb655c4da2980033
shylinux <shylinux@icloud.com>
2021-05-13 10:11:33 +0800

    opt some

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -8,12 +8,6 @@ export GOROOT=$CTX_ROOT/usr/local/go
 
 export PATH=$CTX_ROOT/bin:$CTX_ROOT/usr/publish:$CTX_ROOT/usr/local/bin:$GOROOT/bin:$GOBIN:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-if [ -f ~/.ish/plug.sh ] && source ~/.ish/plug.sh; then
-    require conf.sh
-
-    ish_sys_cli_prompt
-fi
-
 ps aux |grep tmux &>/dev/null && return
 ps aux |grep ice.bin &>/dev/null && return
 cd $CTX_ROOT && source etc/miss.sh
