5988cbe053e4cb6ebc85facba6972598e17daf4a
shaoying <shylinux@icloud.com>
2021-07-17 08:47:34 +0800

    opt git

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -4,10 +4,10 @@ export LC_ALL=en_US.utf-8
 export CTX_ROOT=${CTX_ROOT:=~/contexts}
 
 ish_sys_cli_prompt
+ish_sys_cli_alias vi vim
 ish_sys_cli_alias t "tmux attach"
-
-export PATH=$CTX_ROOT/usr/install/vim81/_install/bin:$PATH
-alias vi=vim
+ish_sys_path_insert $CTX_ROOT/usr/install/vim81/_install/bin
+ish_sys_path_insert $CTX_ROOT/usr/publish
 
 ps aux |grep ice.bin &>/dev/null && return
 ps aux |grep tmux &>/dev/null && return
