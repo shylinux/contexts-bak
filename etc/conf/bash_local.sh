3ea9ed545abd91a3e37acd2486770f9523af8aa8
shaoying <shylinux@icloud.com>
2021-07-18 23:57:55 +0800

    opt some

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -9,6 +9,7 @@ ish_sys_cli_alias t "tmux attach"
 ish_sys_path_insert $CTX_ROOT/usr/install/vim81/_install/bin
 ish_sys_path_insert $CTX_ROOT/usr/publish
 
-ps aux |grep ice.bin &>/dev/null && return
-ps aux |grep tmux &>/dev/null && return
+ps aux |grep -v grep |grep ice.bin &>/dev/null && return
+ps aux |grep -v grep |grep tmux &>/dev/null && return
 cd $CTX_ROOT && source etc/miss.sh
+ish_log_debug "some" "what"
