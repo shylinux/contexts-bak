ed6817695195035b82d06406c9a941d380492870
harveyshao <harveyshao@tencent.com>
2021-09-28 10:16:18 +0800

    opt some

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -6,8 +6,8 @@ export CTX_ROOT=${CTX_ROOT:=~/contexts}
 ish_sys_cli_prompt
 ish_sys_cli_alias vi vim
 ish_sys_cli_alias t "tmux attach"
-ish_sys_path_insert $CTX_ROOT/usr/install/vim81/_install/bin
 ish_sys_path_insert $CTX_ROOT/usr/publish
+ish_sys_path_insert $CTX_ROOT/usr/install/vim81/_install/bin
 
 ps aux |grep -v grep |grep ice.bin &>/dev/null && return
 ps aux |grep -v grep |grep tmux &>/dev/null && return
