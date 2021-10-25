06fe63a2524875725bbe254eaf01688e99019bc3
bergyu <bergyu@tencent.com>
2021-10-25 16:42:21 +0800

    opt relay

--- src/relay.go
+++ src/relay.go
@@ -4,14 +4,12 @@ import (
 	"os"
 	"path"
 
-	ice "shylinux.com/x/icebergs"
-	_ "shylinux.com/x/icebergs/misc/ssh"
-	log "shylinux.com/x/toolkits/logs"
+	"shylinux.com/x/ice"
+	"shylinux.com/x/icebergs/misc/ssh"
 )
 
 func main() {
-	log.LogDisable = true
 	defer func() { recover() }()
-	args := []string{"ssh.connect", "open", "authfile", path.Join(os.Getenv("HOME"), ".ssh/", path.Base(os.Args[0])+".json")}
+	args := []string{ssh.CONNECT, "open", "authfile", path.Join(os.Getenv("HOME"), ".ssh/", path.Base(os.Args[0])+".json")}
 	print(ice.Run(append(args, os.Args[1:]...)...))
 }
