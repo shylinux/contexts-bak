bf5b82c5eda1864eefad8b55e39d0dd4a343f7f6
harveyshao <harveyshao@tencent.com>
2021-11-02 10:13:10 +0800

    opt some

--- etc/conf/bash_local.sh
+++ etc/conf/bash_local.sh
@@ -6,7 +6,11 @@ export CTX_ROOT=${CTX_ROOT:=~/contexts}
 ish_sys_cli_prompt
 ish_sys_cli_alias vi vim
 ish_sys_cli_alias t "tmux attach"
+ish_sys_path_insert $CTX_ROOT/usr/local/bin
 ish_sys_path_insert $CTX_ROOT/usr/publish
+ish_sys_path_insert $CTX_ROOT/bin
+ish_sys_path_insert $PWD/usr/publish
+ish_sys_path_insert $PWD/bin
 
 touch $CTX_ROOT/etc/path
 for line in `cat $CTX_ROOT/etc/path`; do
