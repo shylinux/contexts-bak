f21bd1b5a124b761d96441dda122988468045d33
shylinux <shylinux@163.com>
2021-11-04 15:08:30 +0800

    add m1

--- src/relay.go
+++ src/relay.go
@@ -5,11 +5,11 @@ import (
 	"path"
 
 	"shylinux.com/x/ice"
-	"shylinux.com/x/icebergs/misc/ssh"
+	_ "shylinux.com/x/icebergs/misc/ssh"
 )
 
 func main() {
 	defer func() { recover() }()
-	args := []string{ssh.CONNECT, "open", "authfile", path.Join(os.Getenv("HOME"), ".ssh/", path.Base(os.Args[0])+".json")}
+	args := []string{"ssh.connect", "open", "authfile", path.Join(os.Getenv("HOME"), ".ssh/", path.Base(os.Args[0])+".json")}
 	print(ice.Run(append(args, os.Args[1:]...)...))
 }
