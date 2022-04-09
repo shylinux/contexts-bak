a9c8fbc39ea99f0c557c66f0ffa7a4688bfc8423
harveyshao <harveyshao@tencent.com>
2022-04-09 23:28:21 +0800

    opt some

--- src/debug/go.mod
+++ src/debug/go.mod
@@ -7,6 +7,7 @@ require shylinux.com/x/ice v0.8.9
 require (
 	github.com/kr/pty v1.1.8 // indirect
 	github.com/sqweek/dialog v0.0.0-20220227145630-7a1c9e333fcf
+	github.com/wailsapp/wails v1.16.9 // indirect
 	github.com/webview/webview v0.0.0-20220407190137-85a0d5c36d07 // indirect
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
 	shylinux.com/x/golang-story v0.3.8
@@ -15,6 +16,7 @@ require (
 	shylinux.com/x/mysql-story v0.3.9
 	shylinux.com/x/nginx-story v0.3.9
 	shylinux.com/x/redis-story v0.3.9
+	shylinux.com/x/toolkits v0.5.6
 	shylinux.com/x/webview v0.0.1 // indirect
 )
 
--- src/webview.go
+++ src/webview.go
@@ -2,64 +2,19 @@ package main
 
 import (
 	"os"
-	"strings"
 	"time"
 
 	"shylinux.com/x/ice"
-	"shylinux.com/x/icebergs/base/nfs"
+	"shylinux.com/x/icebergs/misc/webview"
 	kit "shylinux.com/x/toolkits"
-	"shylinux.com/x/webview"
 )
 
-type view struct{ w webview.WebView }
+type view struct{ *webview.WebView }
 
-func (v view) Title(text string)  { v.w.SetTitle(text) }
-func (v view) Webview(url string) { v.w.Navigate(url) }
-func (v view) Open(url string)    { v.w.Navigate(url) }
-func (v view) Terminate()         { v.w.Terminate() }
-func (v view) Close() {
-	if !menu(v.w) {
-		v.Terminate()
-	}
-}
-
-func menu(w webview.WebView) bool {
-	kit.Reflect(view{w: w}, func(name string, value interface{}) { w.Bind(name, value) })
-	list := []string{}
-	ice.Pulse.Cmd(nfs.CAT, "src/webview.txt", func(ls []string, line string) {
-		if len(ls) > 1 {
-			list = append(list, kit.Format(`<button onclick=%s()>%s</button>`, ls[0], ls[0]))
-			w.Bind(ls[0], func() {
-				w.SetSize(1200, 800, webview.HintNone)
-				w.Navigate(ls[1])
-			})
-		}
-	})
-
-	if len(list) == 0 {
-		return false
-	}
-
-	w.SetTitle("contexts")
-	w.SetSize(200, 60*len(list), webview.HintNone)
-	w.Navigate(kit.Format(`data:text/html,
-    <!doctype html>
-    <html>
-	<head><style>button { font-size:24px; margin:10px; width:-webkit-fill-available; display:block; clear:both; }</style></head>
-	<body>%s</body>
-    </html>`, strings.Join(list, "")))
-	return true
-}
 func main() {
 	os.Chdir(kit.HomePath("contexts"))
 	go ice.Run("serve", "start")
 	time.Sleep(time.Second)
 
-	w := webview.New(true)
-	defer w.Destroy()
-	defer w.Run()
-	if !menu(w) {
-		w.SetSize(1200, 800, webview.HintNone)
-		w.Navigate("http://localhost:9020")
-	}
+	webview.Run(func(w *webview.WebView) interface{} { return view{w} })
 }
